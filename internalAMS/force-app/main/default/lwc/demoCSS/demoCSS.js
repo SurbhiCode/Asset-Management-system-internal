e22a65639dfeb92fc810337fefe82f4924229b95
SurbhiCode <158334701+SurbhiCode@users.noreply.github.com>
2024-02-07 01:15:41 +0530

    Delete internalAMS/force-app/main/default/lwc/demoCSS directory

--- internalAMS/force-app/main/default/lwc/demoCSS/demoCSS.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, track } from 'lwc';
-import IMAGEE from "@salesforce/resourceUrl/blurimage";
-
-export default class DemoCSS extends LightningElement {
-    @track img = IMAGEE;
-    
-}
